82e533fffb2816a95bab6ae7dd7097930f313170
ShamimHossain1 <beastb697@gmail.com>
2023-01-26 20:51:40 +0600

    solve problem 4

---
+++ practice-problem-4.js
@@ -0,0 +1,22 @@
+
+
+//------------------------------------------- Practice Problem 4---------------------------------------------------
+// Sarah's mother is teaching her mathematics. She gave Sarah a number and asked her what the
+// remainder would be if she divided the number by 5. Help Sarah write the program.
+
+// Input:
+// The first line of the input contains the number.
+
+// Output:
+// Print the remainder.
+
+// Sample Input: 119
+
+// Output: 4
+
+var num1 = 119;
+var num2= 5;
+
+var remainder = num1 % num2 ;
+console.log(remainder);
+
